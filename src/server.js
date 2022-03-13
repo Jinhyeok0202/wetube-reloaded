edcfa016bdb2affeafa07ed3b0c46fccea6b066c
Yoonjinhyeok0202 <92022572+Yoonjinhyeok0202@users.noreply.github.com>
2022-03-13 20:56:41 +0900

    connect mongodb

--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ import global from "./routers/globalRouters";
 import video from "./routers/videoRouter";
 import user from "./routers/userRouter";
 
-const PORT = 4000;
-
 const app = express();
 const loggerMiddleware = morgan("dev");
 
@@ -17,7 +15,4 @@ app.use("/", global);
 app.use("/videos", video);
 app.use("/users", user);
 
-const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
+export default app;
