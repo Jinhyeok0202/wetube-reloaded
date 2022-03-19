7e377ba68293b8d57329b29bcc5d4b4d59b4ca0d
Yoonjinhyeok0202 <92022572+Yoonjinhyeok0202@users.noreply.github.com>
2022-03-19 20:30:40 +0900

    create video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,6 +2,7 @@ import Video from "../models/Videos";
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
+  console.log(videos);
   return res.render("home", { pageTitle: " Home", videos });
 };
 export const watch = (req, res) => {
@@ -27,9 +28,9 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  const video = new Video({
+  Video.create({
     title,
     description,
     createdAt: Date.now(),
@@ -39,6 +40,6 @@ export const postUpload = (req, res) => {
       rating: 0,
     },
   });
-  console.log(video);
+
   return res.redirect("/");
 };
