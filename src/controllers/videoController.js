f0afd573333d48743638bc946a86138026b23916
Yoonjinhyeok0202 <92022572+Yoonjinhyeok0202@users.noreply.github.com>
2022-04-06 21:34:14 +0900

    deleteVideo or search

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,9 @@
+import { render } from "express/lib/response";
 import Video from "../models/Videos";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
-  console.log(videos);
+  const videos = await Video.find({}).sort({ createAt: "desc" });
+
   return res.render("home", { pageTitle: " Home", videos });
 };
 export const watch = async (req, res) => {
@@ -59,4 +60,16 @@ export const postUpload = async (req, res) => {
       errorMessage: error._message,
     });
   }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
+};
+
+export const search = (req, res) => {
+  const { keyword } = req.query;
+  console.log(keyword);
+  return res.render("search", { pageTitle: "Search" });
 };
