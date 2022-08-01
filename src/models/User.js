1ab571c11cebdcfd2c2d3f922946135c23ded243
Jinhyeok0202 <dbswlsgur4321@gmail.com>
2022-08-01 13:02:29 +0900

    password

--- src/models/User.js
+++ src/models/User.js
@@ -10,9 +10,9 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save", async function () {
-  console.log(this.p1assword);
+  
   this.password = await bcrypt.hash(this.password, 5);
-  console.log(this.password);
+  
 });
 
 const User = mongoose.model("User", userSchema);
